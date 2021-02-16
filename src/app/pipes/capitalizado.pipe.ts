6a91e6797fc98d75c92d84822d038b209bb3e9af
Fernando Santillan <ferdiego88@gmail.com>
2021-02-16 18:37:31 -0500

    pipe

--- src/app/pipes/capitalizado.pipe.ts
+++ src/app/pipes/capitalizado.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class CapitalizadoPipe implements PipeTransform {
 
   transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+    return 'Hola Mundo';
   }
 
 }
