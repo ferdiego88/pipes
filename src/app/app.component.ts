3ae48a60643e4345a3098ca65ffaa3ff990d698e
Fernando Santillan <ferdiego88@gmail.com>
2021-02-16 18:36:17 -0500

    fsv

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,28 @@ import { Component } from '@angular/core';
 export class AppComponent {
   capitan = 'Capitán América';
   tony = 'TONY STARK';
+  diego = 'fErNanDo sAntILlAN';
   fernando = 'Fernando Santillan';
-  arreglo = [1,2,3,4,5,6,7,8,9,10];
+  arreglo = [1, 2 , 3, 4, 5, 6, 7, 8, 9, 10];
   PI = Math.PI;
+  porcentaje = 0.562;
+  moneda = 8500;
+  promesa = new Promise<string>((resolve) => {
+    setTimeout(() => {
+      resolve('Llegó la data');
+    }, 4500);
+  });
+  fecha = new Date();
+  // fechaMoment = moment();
+  idioma = 'fr';
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 500,
+    direccion: {
+      calle: 'Primera',
+      piso: 20
+    }
+  };
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule,LOCALE_ID } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { registerLocaleData} from '@angular/common';
+import localEs from '@angular/common/locales/es';
+import localFr from '@angular/common/locales/fr';
+import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
+registerLocaleData(localEs);
+registerLocaleData(localFr);
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapitalizadoPipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'es'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
